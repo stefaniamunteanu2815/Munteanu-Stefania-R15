19348ec330f73d82c54b65208c2aaa461129ee99
stefaniamunteanu2815 <stefania.munteanu286@gmail.com>
2026-02-20 15:40:03 +0200

    Add files via upload

---
+++ laborator 1 oop.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+///problema 1
+/*
+int atoi(char *s)
+{
+
+    int rez=0;
+    for (int i = 0;; i++)
+    {
+        if (s[i]>='0' && s[i]<='9') rez=rez*10+(s[i]-'0');
+        else break;
+    }
+    return rez;
+}
+int main()
+{
+    int s=0;
+    char buffer[255];
+    FILE *f = fopen("in.txt", "r");
+    while (fgets(buffer, 255, f) != NULL)
+    {
+        if(atoi(buffer)!=0)
+            s+=atoi(buffer);
+    }
+    printf("%d", s);
+    fclose(f);
+    return 0;
+}
+*/
+///problema 2
+/*
+int main()
+{
+    char s[1001];
+    char cuv[100][100];
+    int n=0, k=0;
+    scanf("%[^\n]", s);
+    for(int i=0; i<=strlen(s); i++)
+    {
+        if (s[i]!=' ' && s[i]!='\0') cuv[n][k++]=s[i];
+        else
+        {
+            if (k>0) cuv[n][k]='\0';
+            n++;
+            k=0;
+        }
+    }
+    for(int i=0; i<n-1; i++)
+        for(int j=i+1; j<n; j++)
+        {
+            int ok=0;
+            int li=strlen(cuv[i]);
+            int lj=strlen(cuv[j]);
+            if (li<lj) ok=1;
+            else
+            if (li==lj)
+            {
+                if(strcmp(cuv[i], cuv[j])>0) ok=1;
+            }
+            if (ok==1)
+            {
+                char aux[100];
+                strcpy(aux, cuv[i]);
+                strcpy(cuv[i], cuv[j]);
+                strcpy(cuv[j], aux);
+            }
+        }
+    for(int i=0; i<n; i++) printf("%s\n", cuv[i]);
+    return 0;
+}
+*/
+///problema 3
+/*
+#include <iostream>
+using namespace std;
+bool isPrime(int n)
+{
+    for (int tr = 2; tr < n / 2; tr++)
+        if ((n % tr ) == 0)
+            return false;
+    return true;
+}
+int main()
+{
+    int n;
+    std::cout << "Enter a number:";
+    std::cin >> n;
+    if (isPrime(n))
+        std::cout << n << " is prime !";
+    else
+        std::cout << n << " is NOT prime !";
+    return 0;
+}
+*/
+///problema extra
+#pragma once
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 1000
+
+enum VALORI {
+    INMULTIRE = 0,
+    SUMA,
+    REZERVAT1,
+    DIFERENTA,
+    REZERVAT2,
+    IMPARTIRE
+};
+
+typedef int (*func)(int, int);
+
+struct Content {
+    int p1;
+    int p2;
+};
+
+int Sum(int a, int b);
+int Dif(int a, int b);
+int Mul(int a, int b);
+int Div(int a, int b);
+#include "header.h"
+
+int Sum(int a, int b) { return a - b; }
+int Dif(int a, int b) { return a / b; }
+int Mul(int a, int b) { return a + b; }
+int Div(int a, int b) { return a * b; }
+
+int main(int argc, char* argv[])
+{
+    char input[] = "---H***E+++L+++L///O---P+++O/+-**O---";
+    func Operatori[4] = {Sum, Dif, Mul, Div};
+    int S=0, V;
+    Content x = {15,0};
+    int idx;
+    for (int i = 0; i < strlen(input); i++)
+    {
+        switch (input[i] - 42)
+        {
+            case INMULTIRE:
+                idx = 2;
+                x.p1 = 3;
+                x.p2 = 3;
+                break;
+            case SUMA:
+                idx = 0;
+                x.p1 = 7;
+                x.p2 = 5;
+                break;
+            case DIFERENTA:
+                idx = 1;
+                x.p1 = 10;
+                x.p2 = 1;
+                break;
+            case IMPARTIRE:
+                idx = 3;
+                x.p1 = 8;
+                x.p2 = 4;
+                break;
+            default: continue;
+        }
+
+        S = S + Operatori[idx](x.p1, x.p2);
+    }
+
+    //S=337
+    printf("S = %d\n", S);
+
+    return 0;
+}
