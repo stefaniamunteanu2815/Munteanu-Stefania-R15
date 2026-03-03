3deb77a037f30d13087cee4fb0dafaecdca89710
MunteanuStefaniaR15 <stefania.munteanu286@gmail.com>
2026-03-03 19:04:51 +0400

    Delete Comparators.h

--- Comparators.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Student.h"
-
-int CompareName(const Student& s1, const Student& s2);
-int CompareMathGrade(const Student& s1, const Student& s2);
-int CompareEnglishGrade(const Student& s1, const Student& s2);
-int CompareHistoryGrade(const Student& s1, const Student& s2);
-int CompareAverage(const Student& s1, const Student& s2);
